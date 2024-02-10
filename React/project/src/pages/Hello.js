269b856a806a10b1d2a9d3247c0c2b2426a5e719
Raja21429 <rr912816@gmail.com>
2024-02-10 21:10:47 +0530

    first commit

---
+++ React/project/src/pages/Hello.js
@@ -0,0 +1,62 @@
+function table(){
+    return(
+    
+    <div className="parent">
+            
+        <div className="header"></div>
+            
+        <div className="bottom">
+
+            <div className="left"> </div>
+            <div className="right">
+              <h2>Table</h2>
+                <center>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Column 1</th>
+                            <th>Column 2</th>
+                            <th>Column 3</th>
+                            <th>Column 4</th>
+                            <th>Column 5</th>
+                        </tr>
+                    </thead>
+                    
+                        <tr>
+                            <td>Data 1</td>
+                            <td>Data 2</td>
+                            <td>Data 3</td>
+                            <td>Data 4</td>
+                            <td>Data 5</td>
+                        </tr>
+
+                        <tr>
+                            <td>Data 1</td>
+                            <td>Data 2</td>
+                            <td>Data 3</td>
+                            <td>Data 4</td>
+                            <td>Data 5</td>
+                        </tr>
+
+                        <tr>
+                            <td>Data 1</td>
+                            <td>Data 2</td>
+                            <td>Data 3</td>
+                            <td>Data 4</td>
+                            <td>Data 5</td>
+                        </tr>
+                        
+                    
+                </table>
+                </center>
+            </div>
+
+        </div>
+
+
+    
+    </div>
+);
+}
+
+export default table;
